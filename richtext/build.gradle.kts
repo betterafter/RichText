f98ef1fe7c80ad205f59e7d18f3ede957f7d29f1
keykat <koin12000@naver.com>
2024-06-21 17:02:38 +0900

    [feature] 버전 초기화

--- richtext/build.gradle.kts
+++ richtext/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
 }
 
@@ -8,10 +8,13 @@ android {
     compileSdk = 34
 
     defaultConfig {
+        applicationId = "com.keykat.richtext"
         minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
